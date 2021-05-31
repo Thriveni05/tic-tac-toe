7cb411e8ab57187192322a3ffe736502488b8922
Thriveni05 <85114730+Thriveni05@users.noreply.github.com>
2021-05-31 19:10:38 +0530

    Add files via upload
    second commit

---
+++ tic_tac_toe.py
@@ -0,0 +1,103 @@
+board={'T1':' ','T2':' ','T3':' ','M1':' ','M2':' ','M3':' ','D1':' ','D2':' ','D3':' '}
+player=1
+total_moves=0
+end_check=0
+kittu=0
+def check():
+    if board['T1']=='X' and board['T2']=='X'and board['T3']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['M1']=='X' and board['M2']=='X'and board['M3']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['D1']=='X' and board['D2']=='X'and board['D3']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['T1']=='X' and board['M2']=='X'and board['D3']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['T3']=='X' and board['M2']=='X'and board['D1']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['T1']=='X' and board['M1']=='X'and board['D1']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['T2']=='X' and board['M2']=='X'and board['D2']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['T3']=='X' and board['M3']=='X'and board['D3']=='X':
+        print("PLAYER ONE WON!")
+        return 1
+    if board['T1']=='O' and board['T2']=='O'and board['T3']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['M1']=='O' and board['M2']=='O'and board['M3']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['D1']=='O' and board['D2']=='O'and board['D3']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['T1']=='O' and board['M2']=='O'and board['D3']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['T3']=='O' and board['M2']=='O'and board['D1']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['T1']=='O' and board['M1']=='O'and board['D1']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['T2']=='O' and board['M2']=='O'and board['D2']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+    if board['T3']=='O' and board['M3']=='O'and board['D3']=='O':
+        print("PLAYER TWO WON!!")
+        return 1
+print('T1|T2|T3')
+print('-+-+-')
+print('M1|M2|M3')
+print('-+-+-')
+print('D1|D2|D3')
+print('******')
+
+while True:
+    print(board['T1']+'|'+board['T2']+'|'+board['T3'])
+    print('-+-+-')
+    print(board['M1']+'|'+board['M2']+'|'+board['M3'])
+    print('-+-+-')
+    print(board['D1']+'|'+board['D2']+'|'+board['D3'])
+    print('-+-+-')
+    end_check=check()
+    if total_moves==9 or end_check==1:
+        p=input("If you want to play again")
+        if(p=="No"):
+            break
+        else: 
+            board={'T1':' ','T2':' ','T3':' ','M1':' ','M2':' ','M3':' ','D1':' ','D2':' ','D3':' '}
+            total_moves=0
+            player=1
+    while True:
+        if player==1:
+            p1_input=input('player one')
+            if p1_input.upper() in board and board[p1_input.upper()]==' ':
+                board[p1_input.upper()]='X'
+                player=2
+                break
+            else:
+                print("Invalid")
+                continue
+
+        else:
+            p2_input=input('player two')
+            if p2_input.upper() in board and board[p2_input.upper()]==' ':
+                board[p2_input.upper()]='O'
+                player=1
+                break
+            
+            else:
+                print("Invalid")
+                continue
+                
+    total_moves+=1
+    print("total moves:",total_moves)
+    print()
+    
